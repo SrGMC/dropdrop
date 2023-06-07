5d8026c57653d7ca4e1b82de8b2cdb83de97cbba
Alvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2023-06-07 09:25:07 +0000

    Add GitHub link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import 'carbon-components-svelte/css/all.css';
 	import '../styles.css';
-	import { Content, Header, SkipToContent } from 'carbon-components-svelte';
+	import { Content, Header, HeaderActionLink, HeaderGlobalAction, HeaderUtilities, SkipToContent } from 'carbon-components-svelte';
+	import { LogoGithub, SettingsAdjust } from 'carbon-icons-svelte';
 
 	let theme = 'white'; // "white" | "g10" | "g80" | "g90" | "g100"
 </script>
 
-<Header company="DropDrop" href="/">
+<Header platformName="DropDrop" href="/">
 	<svelte:fragment slot="skip-to-content">
 		<SkipToContent />
 	</svelte:fragment>
+	<HeaderUtilities>
+		<HeaderActionLink aria-label="Settings" icon={LogoGithub} href="https://github.com/SrGMC/dropdrop" />
+	</HeaderUtilities>
 </Header>
 
 <Content>
