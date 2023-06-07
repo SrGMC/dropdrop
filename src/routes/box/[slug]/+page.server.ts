3d468817debc7f54d7d8c5cc587b79aa57be8ec1
Alvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2023-06-07 07:28:45 +0000

    Disable prerendering on dynamic routes

--- src/routes/box/[slug]/+page.server.ts
+++ src/routes/box/[slug]/+page.server.ts
@@ -1,5 +1,7 @@
 import { listDirectory } from '$lib/files/node';
 
+export const prerender = false;
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, url }) {
 	const boxId: string = params.slug;
